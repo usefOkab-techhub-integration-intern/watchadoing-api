af7a233f6aed731de5767272c641020de03379cd
usefSaeed <youssef.okab@gmail.com>
2024-09-08 11:42:13 +0300

    db connected using .env

--- src/datasources/watcha-doing-db.datasource.ts
+++ src/datasources/watcha-doing-db.datasource.ts
@@ -2,15 +2,19 @@ import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 import * as dotenv from 'dotenv';
 
+require('dotenv').config();
+
+
 const config = {
   name: 'WatchaDoingDB',
   connector: 'mysql',
+  url: '',
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
-};
+}
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
